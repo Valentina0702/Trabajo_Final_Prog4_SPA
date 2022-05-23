4690e7a8bd4b59f5bb347d87682f6fe934346dd9
Valentina Díaz <vdiaz74277@umanizales.edu.co>
2022-05-22 21:52:38 -0500

    2 Commit: Etiquetas meta

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import MetaTags from 'react-meta-tags';
+
+class Component1 extends React.Component {
+  render() {
+    return (
+        <div class="wrapper">
+          <MetaTags>
+            <title>Page 1</title>
+            <meta id="meta-Author" name="Author" content="Valentina Díaz & Nicolas Trujillo" />
+            <meta id="meta-description" content="Hoja de Vida SPA" />
+            <meta id="meta-keywords" content="empresa, estudios, experiencia, referencias" />
+            <meta charset="utf-8"></meta>
+          </MetaTags>
+          <div class="content"> Some Content </div>
+        </div>
+      )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
