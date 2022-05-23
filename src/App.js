e2b087f49d469f3688011bc9fd9ac186f13ceca6
Valentina Díaz <vdiaz74277@umanizales.edu.co>
2022-05-22 21:58:18 -0500

    3 Commit: Sin Etiquetas meta corrigiendo error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import MetaTags from 'react-meta-tags';
-
-class Component1 extends React.Component {
-  render() {
-    return (
-        <div class="wrapper">
-          <MetaTags>
-            <title>Page 1</title>
-            <meta id="meta-Author" name="Author" content="Valentina Díaz & Nicolas Trujillo" />
-            <meta id="meta-description" content="Hoja de Vida SPA" />
-            <meta id="meta-keywords" content="empresa, estudios, experiencia, referencias" />
-            <meta charset="utf-8"></meta>
-          </MetaTags>
-          <div class="content"> Some Content </div>
-        </div>
-      )
-  }
-}
 
 function App() {
   return (
